8ad5d8f7711cf3f407a20e6226b6bf410d0688ea
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-05-16 16:12:32 +0600

    basic setup of the project done

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 
-app.get("/", (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   const a = 10;
+
   res.send(a);
 });
 
