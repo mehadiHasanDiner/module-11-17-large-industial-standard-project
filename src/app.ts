0b0fd7683ff52cbb1f22ffacfb9c4809d3fd307a
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-05-16 12:59:46 +0600

    es-lint all done

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,9 @@
 import express, { Application, Request, Response } from "express";
 const app: Application = express();
-const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
   const a = 10;
   res.send(a);
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
 });
 
 export default app;
