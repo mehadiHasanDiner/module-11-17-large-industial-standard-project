6314339bb60d3ee1d64cd7c1da7fddceafd39aad
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-05-17 21:46:51 +0600

    done add data get data

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
+import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import { StudentRoutes } from './app/modules/student/student.route';
+
 const app: Application = express();
 
-app.get('/', (req: Request, res: Response) => {
-  const a = 10;
+// parser
+app.use(express.json());
+app.use(cors());
 
-  res.send(a);
-});
+// /api/v1/students/create-student
+
+// application route
+app.use('/api/v1/students', StudentRoutes);
+
+const getAController = (req: Request, res: Response) => {
+  // const a = 10;
+  res.send({ status: 'success' });
+};
+
+app.get('/', getAController);
 
 export default app;
