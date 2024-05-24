59cb448df8eeb05ac8644632776f0a8a3c649bac
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-05-25 02:54:25 +0600

    video-9 done

--- src/app/modules/user/user.controllers.ts
+++ src/app/modules/user/user.controllers.ts
@@ -1,26 +1,15 @@
+import { UserServices } from './user.service';
+
 const createStudent = async (req: Request, res: Response) => {
   try {
     // creating a schema for validation using zod
 
-    const { student: studentData } = req.body;
+    const { password, student: studentData } = req.body;
 
-    // data validation using joi
-    // const { error, value } = studentValidationSchema.validate(studentData);
-
-    // data validation using zod
-    const zodParsedData = studentValidationSchema.parse(studentData);
-
-    const result = await StudentServices.createStudentIntoDB(zodParsedData);
-
-    // console.log({ error }, { value });
-
-    // if (error) {
-    //   res.status(500).json({
-    //     success: false,
-    //     message: 'something went wrong',
-    //     data: error.details,
-    //   });
-    // }
+    const result = await UserServices.createStudentIntoDB(
+      password,
+      studentData,
+    );
 
     res.status(200).json({
       success: true,
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,6 +2,6 @@ import express from 'express';
 
 const router=express.Router();
 
-router.post('/create-student', )
+router.post('/create-student', UserControllers.createStudent)
 
 export const UserRoutes = router
