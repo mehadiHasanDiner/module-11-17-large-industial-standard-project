fb800c616eb7507b89b558c2ef641a6385a3c487
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-06-02 07:12:37 +0600

    module2 finished

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -36,16 +36,41 @@ const getSingleStudentsFromDB = async (id: string) => {
 const updateStudentsIntoDB = async (id: string, payload: Partial<TStudent>) => {
   const { name, guardian, localGuardian, ...remainingStudentData } = payload;
 
+  const modifiedUpdatedData: Record<string, unknown> = {
+    ...remainingStudentData,
+  };
+
   /*
     guardian:{
     fatherOccupation:"Teacher"
     }
-
     guardian.fatherOccupation = Teacher
    
   */
 
-  const result = await Student.findOneAndUpdate({ id }, payload);
+  if (name && Object.keys(name).length) {
+    for (const [key, value] of Object.entries(name)) {
+      modifiedUpdatedData[`name.${key}`] = value;
+    }
+  }
+
+  if (guardian && Object.keys(guardian).length) {
+    for (const [key, value] of Object.entries(guardian)) {
+      modifiedUpdatedData[`guardian.${key}`] = value;
+    }
+  }
+
+  if (localGuardian && Object.keys(localGuardian).length) {
+    for (const [key, value] of Object.entries(localGuardian)) {
+      modifiedUpdatedData[`localGuardian.${key}`] = value;
+    }
+  }
+  console.log(modifiedUpdatedData);
+
+  const result = await Student.findOneAndUpdate({ id }, modifiedUpdatedData, {
+    new: true,
+    runValidators: true,
+  });
 
   return result;
 };
